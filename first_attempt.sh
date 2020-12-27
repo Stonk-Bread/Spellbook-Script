8d07fcdb8105a5dbe6edbd39ec6cac0b710fa8fc
Jay <StonkBread@gmail.com>
2020-12-27 08:18:54 -0500

    original file will now be deleted upon completion. More changes to come.

--- first_attempt.sh
+++ first_attempt.sh
@@ -18,6 +18,19 @@ else
     echo "Your casting was unsuccessful $(whoami)."
 fi
 
+if [ " $a == $b " ]
+then 
+    echo "The original text has been consumed." 
+else   
+    echo "Something went wrong, the original text has not been consumed."
+fi
+
+
+if [ "$a == $b" ]
+then 
+    rm spellbook.txt
+fi
+
 # I am attempting to rot13 the content of spellbook.txt and output it to a new 
 #   file named translated_spellbook.txt
 # I then want to compare the amount of characters between the two, as a way to 
@@ -27,3 +40,7 @@ fi
 # The new file is created, with the appropriately formatted text, and the 
 # character number does in fact match. 
 
+#fixed issues, and have implemented the rest of the script. 
+# The original file will now be deleted after comparing the number of characters.
+#   This does the job, but is not a meaningful test. 
+# Will add a filesize comparison in addition to character comparison, to add a meaningful layer, as well as see if I can layer without breaking it.
