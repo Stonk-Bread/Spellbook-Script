4c3188194fc0eb7cfd88583b210c34be3a16147e
Stonk-Bread <76273845+Stonk-Bread@users.noreply.github.com>
2020-12-27 01:41:28 -0500

    Update first_attempt.sh
    commented what I've done and what is not working so far

--- first_attempt.sh
+++ first_attempt.sh
@@ -18,3 +18,12 @@ else
     echo "Your casting was unsuccessful."
 fi
 
+# I am attempting to rot13 the content of spellbook.txt and output it to a new 
+#   file named translated_spellbook.txt
+# I then want to compare the amount of characters between the two, as a way to 
+#    verify that the process worked. 
+# I'm getting " /first_attempt.sh: 14: [: 18: unexpected operator
+# Your casting was unsuccessful. " 
+# The new file is created, with the appropriately formatted text, and the 
+# character number does in fact match. 
+
